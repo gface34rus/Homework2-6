9b1890a10c934f4331ce6628a119a8cdaf465db1
gface34rus <89259112970@mail.ru>
2024-08-07 21:11:05 +0300

    Initial commit

---
+++ src/main/java/pro/sky/Homework2_6/Homework26Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework2_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework26Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Homework2_6/Homework26ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework2_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
